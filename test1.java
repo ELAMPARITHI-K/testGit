266d01488cf01c8b367de9008da50d262ae4103c
Elamparithi <elamparithik007@gmail.com>
2022-01-04 10:50:21 +0530

    bt2 fresh

--- test1.java
+++ test1.java
@@ -1,18 +1,6 @@
 class Test1{
   public static void main(String[] args){
     System.out.println("Elam- test1 java file modified for commit 1");
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    System.out.println("Elam- bt2 check");
-=======
-    System.out.println("Elam- master1 check");
->>>>>>> 987bc12143e00a33e7aadee551902f40cef1d36f
-=======
-    System.out.println("Elam- master check");
->>>>>>> 70fcb6e012de8f662264e794d886793b9c30caed
-=======
-    System.out.println("Elam- master2 check");
->>>>>>> 07261d2f6c61a53153fb10c4b8bb44cf2baf32d0
+    System.out.println("bt2");
   }
 }
--- test1_REMOTE_2008.java
+++
@@ -1,6 +0,0 @@
-class Test1{
-  public static void main(String[] args){
-    System.out.println("Elam- test1 java file modified for commit 1");
-    System.out.println("Elam- master2 check");
-  }
-}
