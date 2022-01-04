9b3ba7dd5b055fdd6d14368255f2c480a7700f14
Elamparithi <elamparithik007@gmail.com>
2022-01-04 10:54:58 +0530

    bt1 fresh

--- test1.java
+++ test1.java
@@ -1,12 +1,6 @@
 class Test1{
   public static void main(String[] args){
     System.out.println("Elam- test1 java file modified for commit 1");
-<<<<<<< HEAD
-    System.out.println("Elam- branchTest1 check");
-    System.out.println("Elam- branchTest2 check");
-    System.out.println("Elam- master check 1");
-=======
-    System.out.println("Elam- master check");
->>>>>>> d20d7713fb8af6d97ba313dfa61984643f34bf4b
+    System.out.println("bt1");
   }
 }
